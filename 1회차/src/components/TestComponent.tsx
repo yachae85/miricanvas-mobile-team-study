b22ab5666575f3417552a65fbcad9711f2f3468d
yachae85 <yachae85@github.com>
2024-09-16 16:20:50 +0900

    feat: useState

--- 1회차/src/components/TestComponent.tsx
+++ 1회차/src/components/TestComponent.tsx
@@ -1,15 +1,29 @@
 import { useState } from '../custom_hooks/useState';
 
 export const TestComponent = () => {
-  const [] = useState();
-  const [] = useState();
+  const [counter, setCounter] = useState<number>(0);
+  const [anotherCounter, setAnotherCounter] = useState<number>(0);
+
+  const onClick = () => {
+    setCounter(counter + 1);
+  };
+
+  const onClickAnother = () => {
+    setAnotherCounter(anotherCounter + 1);
+  };
 
   return (
     <div>
       <p>uk useState test</p>
-      <div>
-        <span>counter: </span>
-        <span>another counter: </span>
+      <div className="container">
+        <div>
+          <button onClick={onClick}>click</button>
+          <span>counter: {counter}</span>
+        </div>
+        <div>
+          <button onClick={onClickAnother}>click</button>
+          <span>another counter: {anotherCounter}</span>
+        </div>
       </div>
     </div>
   );
--- 1회차/src/custom_hooks/useState.ts
+++ 1회차/src/custom_hooks/useState.ts
@@ -1,3 +1,25 @@
-export const useState = () => {
-  return [];
+import { testRender } from '../render';
+
+let key = 0; // state의 index에 접근하기 위한 key
+const state = [] as any[]; // state를 관리하는 배열
+
+export const useState = <T>(initialValue: T): [T, (newState: T) => void] => {
+  const currentKey = key; // useState가 호출됐을때 currentKey = 몇번째 index에 접근해야되는지
+  if (state[currentKey] === undefined) {
+    // 현재에 해당하는 값이 없으면 initialValue로 초기화
+    state[currentKey] = initialValue;
+  }
+
+  const setState = (newState: T) => {
+    // setState 동작시 state[currentKey 업데이트]
+    state[currentKey] = newState;
+    // state update이후 key를 다시 0으로 초기화
+    key = 0;
+    // reRendering
+    testRender();
+  };
+
+  key++; // 다음 useState 호출을 위해 키 증가
+
+  return [state[currentKey], setState];
 };
