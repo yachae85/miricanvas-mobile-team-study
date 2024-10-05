dd762546de40c9b2ae482c1ea0854458f17c4eb5
yachae85 <yachae85@github.com>
2024-10-05 16:03:56 +0900

    feat: useCallback

--- 1회차/src/components/TestComponent.tsx
+++ 1회차/src/components/TestComponent.tsx
@@ -1,12 +1,18 @@
 import { useMemo } from '../custom_hooks/useMemo';
 import { useEffect } from '../custom_hooks/useEffect';
 import { useState } from '../custom_hooks/useState';
+import { useCallback } from '../custom_hooks/useCallback';
 
 export const TestComponent = () => {
   const [counter, setCounter] = useState<number>(0);
   const [anotherCounter, setAnotherCounter] = useState<number>(0);
   const [thirdCounter, setThirdCounter] = useState<number>(0);
   const tempMemo = useMemo(() => Boolean(counter % 2), [counter]);
+  const tempFunc = useCallback(() => {
+    console.log('counter is', counter);
+  }, [counter]);
+
+  const [temp, setTemp] = useState(tempFunc);
 
   const onClick = () => {
     setCounter(counter + 1);
@@ -20,16 +26,16 @@ export const TestComponent = () => {
     setThirdCounter(thirdCounter + 1);
   };
 
-  useEffect(() => {
-    // console.log('counter or antherCounter changed');
-    // console.log('counter: ', counter);
-    // console.log('anotherCounter: ', anotherCounter);
-  }, [counter, anotherCounter]);
+  // useEffect(() => {
+  // console.log('counter or antherCounter changed');
+  // console.log('counter: ', counter);
+  // console.log('anotherCounter: ', anotherCounter);
+  // }, [counter, anotherCounter]);
 
-  useEffect(() => {
-    console.log('custom useMemo');
-    console.log('meoization value: ', tempMemo);
-  }, [tempMemo]);
+  // useEffect(() => {
+  //   console.log('custom useMemo');
+  //   console.log('meoization value: ', tempMemo);
+  // }, [tempMemo]);
 
   return (
     <div>
@@ -46,6 +52,20 @@ export const TestComponent = () => {
         <div>
           <button onClick={onClickThird}>click</button>
           <span>third counter: {thirdCounter}</span>
+        </div>
+        <div>
+          <span>what is counter?</span>
+          <button onClick={tempFunc}>click</button>
+          <button
+            onClick={() => {
+              console.log(temp === tempFunc);
+              if (temp !== tempFunc) {
+                setTemp(tempFunc);
+              }
+            }}
+          >
+            isSame?
+          </button>
         </div>
       </div>
     </div>
